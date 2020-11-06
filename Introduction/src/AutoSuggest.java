e97334a5af114c800c239118fa528b8d8f1f417c
Lisipriya <aplisipriya1998@gmail.com>
2020-11-06 09:52:39 +0530

    Add items to cart

--- Introduction/src/AutoSuggest.java
+++ Introduction/src/AutoSuggest.java
@@ -9,45 +9,40 @@ public class AutoSuggest {
 
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
-		System.setProperty("webdriver.chrome.driver","C:\\Program Files\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
-		
-		//Radio Button
+
+		// Radio Button
 		System.out.println(driver.findElement(By.id("Div1")).getAttribute("style"));
 		driver.findElement(By.cssSelector("input[value='RoundTrip']")).click();
 		System.out.println(driver.findElement(By.id("Div1")).getAttribute("style"));
-		if(driver.findElement(By.id("Div1")).getAttribute("style").contains("1"))
-		{
+		if (driver.findElement(By.id("Div1")).getAttribute("style").contains("1")) {
 			Assert.assertTrue(true);
-		}
-		else
-		{
+		} else {
 			Assert.assertTrue(false);
 		}
-		
-		//checkbox
+
+		// checkbox
 		System.out.println(driver.findElement(By.cssSelector("input[id*='friendsandfamily']")).isSelected());
 		driver.findElement(By.cssSelector("input[id*='friendsandfamily']")).click();
 		Assert.assertTrue(driver.findElement(By.cssSelector("input[id*='friendsandfamily']")).isSelected());
-		
-		//count of all checkboxes
+
+		// count of all checkboxes
 		System.out.println(driver.findElements(By.cssSelector("input[type='checkbox']")).size());
-		
-		//Autosuggest
+
+		// Autosuggest
 		driver.findElement(By.id("autosuggest")).sendKeys("ind");
-		Thread.sleep(1000L);//waits for 1 second
+		Thread.sleep(1000L);// waits for 1 second
 		List<WebElement> options = driver.findElements(By.cssSelector("li[class='ui-menu-item'] a"));
-		
-		for (WebElement option :options)
-		{
-			if(option.getText().equalsIgnoreCase("India"))
-			{
+
+		for (WebElement option : options) {
+			if (option.getText().equalsIgnoreCase("India")) {
 				System.out.println(option.getText());
 				Assert.assertEquals(option.getText(), "India");
-				option.click();	
+				option.click();
 				break;
-				
+
 			}
 		}
 	}
